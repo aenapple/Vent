85e0b74de157f241bd797878a53c8073a731c219
Evgeniy Alekseev <evgeniy_alekseev@yahoo.ca>
2020-11-27 00:40:30 -0500

    Changed charts.

--- Parser.py
+++ Parser.py
@@ -4,18 +4,18 @@ import matplotlib.pyplot as plt
 def transform_data(file_name, t_array, s_array):
     file_input = open(file_name, 'r')
     for line in file_input:
-        x = line.find('11/25/2020')
+        x = line.find('11/26/2020')
         if x != 0:
             continue
 
-        str_record = line.replace('11/25/2020-23:', '')
+        str_record = line.replace('11/26/2020-', '')
         # minutes = int(str_record[0:2])
         # seconds = int(str_record[3:5])
-        s_array.append(int(str_record[3:5]) + (int(str_record[0:2]) * 60))
+        s_array.append(int(str_record[6:8]) + (int(str_record[3:5]) * 60) + (int(str_record[0:2]) * 3600))
         # t1 = int(str_record[13:17])
         # t2 = int(str_record[18:22])
         # t3 = int(str_record[23:27])
-        t = (int(str_record[13:17]) + int(str_record[18:22]) + int(str_record[23:27])) / 300
+        t = (int(str_record[16:20]) + int(str_record[21:25]) + int(str_record[26:30])) / 300
         t_array.append(t)
         print(str_record, end='')
 
@@ -36,11 +36,11 @@ if __name__ == '__main__':
     s1 = []
     s2 = []
     # s3 = []
-    transform_data('LogTemperature_4.txt', t1, s1)  # room-sensor
-    transform_data('LogTemperature_C.txt', t2, s2)  # vent-sensor
-    # transform_data('LogTemperature_6.txt', t1, s1)  # room-sensor
+    # transform_data('LogTemperature_4.txt', t1, s1)  # room-sensor
+    # transform_data('LogTemperature_C.txt', t2, s2)  # vent-sensor
+    transform_data('LogTemperature_6.txt', t1, s1)  # room-sensor
     # transform_data('LogTemperature_9.txt', t2, s2)  # vent-sensor
-    # transform_data('LogTemperature_B.txt', t3, s3)  # vent-sensor
+    transform_data('LogTemperature_B.txt', t2, s2)  # vent-sensor
 
     fig, ax = plt.subplots()
     ax.plot(s1, t1, label="room-sensor")
@@ -49,8 +49,8 @@ if __name__ == '__main__':
 
     # ax.set_xlim(0, 5)
     # ax.set_ylim(0, 10)
-    # ax.set(xlabel='time', ylabel='temperature', title='Bedroom')
-    ax.set(xlabel='time', ylabel='temperature', title='Cabinet')
+    ax.set(xlabel='time', ylabel='temperature', title='Bedroom')
+    # ax.set(xlabel='time', ylabel='temperature', title='Cabinet')
     ax.grid()
 
     # fig, ax = plt.subplots(2, 1)
