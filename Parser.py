62542a355835df2f25258d940d9fea45db286d89
Evgeniy Alekseev <evgeniy_alekseev@yahoo.ca>
2020-11-30 22:12:35 -0500

    Added: 1. remove line with error 2. create CSV-file.

--- Parser.py
+++ Parser.py
@@ -5,22 +5,24 @@ def transform_data(file_name):
     t_array = []
     s_array = []
     file_input = open(file_name, 'r')
+    file_csv = open(file_name.replace('txt', 'csv'), 'w')
     for line in file_input:
-        x = line.find('11/27/2020')
-        if x != 0:
+        if (line.find('2020') < 0) or (line.find('ERR') >= 0) or (line.find('OK') >= 0):  # empty or error line
             continue
 
-        str_record = line.replace('11/27/2020-' , '')
-        # minutes = int(str_record[0:2])
-        # seconds = int(str_record[3:5])
-        s_array.append(int(str_record[6:8]) + (int(str_record[3:5]) * 60) + (int(str_record[0:2]) * 3600))
-        # t1 = int(str_record[13:17])
-        # t2 = int(str_record[18:22])
-        # t3 = int(str_record[23:27])
-        # t = int(str_record[16:20] + int(str_record[21:25]) + int(str_record[26:30])) / 300
-        t_array.append((int(str_record[16:20]) + int(str_record[21:25]) + int(str_record[26:30])) / 300)
+        file_csv.write(line[0:10] + ',')
+        str_record = line.replace(line[0:11], '')  # remove Date
+
+        seconds = int(str_record[6:8]) + (int(str_record[3:5]) * 60) + (int(str_record[0:2]) * 3600)
+        s_array.append(seconds)
+        file_csv.write(str(seconds) + ',')
+
+        t = (int(str_record[16:20]) + int(str_record[21:25]) + int(str_record[26:30])) / 300
+        t_array.append(t)
+        file_csv.write(str(t) + ',' + '\n')
         print(str_record, end='')
 
+    file_csv.close()
     file_input.close()
     return t_array, s_array
 
